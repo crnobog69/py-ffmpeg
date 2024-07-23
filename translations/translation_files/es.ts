5386141ede4dff6ade14b31c16cd38384642e8e9
Црнобог <79972075+crnobog69@users.noreply.github.com>
2024-07-23 21:31:55 +0200

    Нови Превод(и)|New Translation(s)

---
+++ translations/translation_files/es.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+<name>FFmpegGUI</name>
+<message>
+<location filename="../py-ffmpeg.py" line="375"/>
+<source>PyFFmpeg | English</source>
+<translation>PyFFmpeg | Español</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="376"/>
+<source>Select a file:</source>
+<translation>Seleccionar un archivo:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="379"/>
+<source>Browse</source>
+<translation>Examinar</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="378"/>
+<source>Save converted file as:</source>
+<translation>Guardar archivo convertido como:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="380"/>
+<source>Select output format:</source>
+<translation>Seleccionar formato de salida:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="381"/>
+<source>Convert</source>
+<translation>Convertir</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="383"/>
+<source>Ready</source>
+<translation>Listo</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="384"/>
+<source>About</source>
+<translation>Acerca de</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="382"/>
+<source>Change Language</source>
+<translation>Cambiar idioma</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="386"/>
+<source>Video</source>
+<translation>Vídeo</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="387"/>
+<source>Audio</source>
+<translation>Audio</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="388"/>
+<source>Image</source>
+<translation>Imagen</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="389"/>
+<source>Other</source>
+<translation>Otro</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="180"/>
+<source>Open file</source>
+<translation>Abrir archivo</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="190"/>
+<source>Save file</source>
+<translation>Guardar archivo</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="227"/>
+<source>Error</source>
+<translation>Error</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="223"/>
+<source>Please select an input file.</source>
+<translation>Por favor, selecciona un archivo de entrada.</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="227"/>
+<source>Please select an output file.</source>
+<translation>Por favor, selecciona un archivo de salida.</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="230"/>
+<source>Converting...</source>
+<translation>Convirtiendo...</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="241"/>
+<source>Conversion completed!</source>
+<translation>¡Conversión completada!</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="275"/>
+<source>About PyFFmpeg</source>
+<translation>Acerca de PyFFmpeg</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="278"/>
+<source>&lt;h1&gt;PyFFmpeg&lt;/h1&gt;&lt;p&gt;Version 0.0.0.4&lt;/p&gt;&lt;p&gt;A simple FFmpeg GUI built with PyQt5.&lt;/p&gt;&lt;p&gt;Made by Црнобог | Crnobog&lt;/p&gt;</source>
+<translation>&lt;h1&gt;PyFFmpeg&lt;/h1&gt;&lt;p&gt;Versión 0.0.0.4&lt;/p&gt;&lt;p&gt;Una interfaz gráfica sencilla para FFmpeg construida con PyQt5.&lt;/p&gt;&lt;p&gt;Hecho por Црнобог | Crnobog&lt;/p&gt;</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="296"/>
+<source>GitHub</source>
+<translation>GitHub</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="298"/>
+<source>Close</source>
+<translation>Cerrar</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="347"/>
+<source>Select Language</source>
+<translation>Seleccionar idioma</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="347"/>
+<source>Language:</source>
+<translation>Idioma:</translation>
+</message>
+</context>
+</TS>
---
+++ translations/translation_files/kz.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+<name>FFmpegGUI</name>
+<message>
+<location filename="../py-ffmpeg.py" line="375"/>
+<source>PyFFmpeg | English</source>
+<translation>PyFFmpeg | Қазақша</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="376"/>
+<source>Select a file:</source>
+<translation>Файлды таңдаңыз:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="379"/>
+<source>Browse</source>
+<translation>Шолу</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="378"/>
+<source>Save converted file as:</source>
+<translation>Конверттелген файлды сақтау:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="380"/>
+<source>Select output format:</source>
+<translation>Шығыс форматын таңдаңыз:</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="381"/>
+<source>Convert</source>
+<translation>Конверттеу</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="383"/>
+<source>Ready</source>
+<translation>Дайын</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="384"/>
+<source>About</source>
+<translation>Туралы</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="382"/>
+<source>Change Language</source>
+<translation>Тілді өзгерту</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="386"/>
+<source>Video</source>
+<translation>Бейне</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="387"/>
+<source>Audio</source>
+<translation>Аудио</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="388"/>
+<source>Image</source>
+<translation>Сурет</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="389"/>
+<source>Other</source>
+<translation>Басқа</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="180"/>
+<source>Open file</source>
+<translation>Файлды ашу</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="190"/>
+<source>Save file</source>
+<translation>Файлды сақтау</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="227"/>
+<source>Error</source>
+<translation>Қате</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="223"/>
+<source>Please select an input file.</source>
+<translation>Кіріс файлын таңдаңыз.</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="227"/>
+<source>Please select an output file.</source>
+<translation>Шығыс файлын таңдаңыз.</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="230"/>
+<source>Converting...</source>
+<translation>Конверттеу...</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="241"/>
+<source>Conversion completed!</source>
+<translation>Конверттеу аяқталды!</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="275"/>
+<source>About PyFFmpeg</source>
+<translation>PyFFmpeg туралы</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="278"/>
+<source>&lt;h1&gt;PyFFmpeg&lt;/h1&gt;&lt;p&gt;Version 0.0.0.4&lt;/p&gt;&lt;p&gt;A simple FFmpeg GUI built with PyQt5.&lt;/p&gt;&lt;p&gt;Made by Црнобог | Crnobog&lt;/p&gt;</source>
+<translation>&lt;h1&gt;PyFFmpeg&lt;/h1&gt;&lt;p&gt;Нұсқа 0.0.0.4&lt;/p&gt;&lt;p&gt;PyQt5 көмегімен жасалған қарапайым FFmpeg графикалық интерфейсі.&lt;/p&gt;&lt;p&gt;Црнобог жасаған | Crnobog&lt;/p&gt;</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="296"/>
+<source>GitHub</source>
+<translation>GitHub</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="298"/>
+<source>Close</source>
+<translation>Жабу</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="347"/>
+<source>Select Language</source>
+<translation>Тілді таңдау</translation>
+</message>
+<message>
+<location filename="../py-ffmpeg.py" line="347"/>
+<source>Language:</source>
+<translation>Тіл:</translation>
+</message>
+</context>
+</TS>
